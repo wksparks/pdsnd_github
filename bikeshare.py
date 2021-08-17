a8d419306c2366ee64782190e2d67741e4b932fb
Will Sparks <wsparks@sparkstechnicalservice.com>
2021-08-17 17:15:29 -0400

    refactor: clarify print statement for frequent travel

--- bikeshare.py
+++ bikeshare.py
@@ -140,7 +140,7 @@ def load_data(city, month, day):
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print('\nCalculating The Most Frequent Times of Travel\n')
     start_time = time.time()
 
     # display the most common month
